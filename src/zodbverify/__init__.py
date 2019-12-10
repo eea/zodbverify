db657d8235b552eca855fd22d936d524b4baa058
Alin Voinea <contact@avoinea.com>
2019-12-10 18:45:21 +0200

    Patch to provide plone.dexterity.schema.generated utility within zodbverify

---
+++ src/zodbverify/__init__.py
@@ -0,0 +1,14 @@
+""" Fix plone.dexterity.schema.generated exception
+"""
+import logging
+from zope.component.hooks import getSiteManager
+logger = logging.getLogger("zodbverify")
+sm = getSiteManager()
+
+try:
+    from plone.dexterity.schema import SchemaModuleFactory
+    sm.registerUtility(factory=SchemaModuleFactory, name="plone.dexterity.schema.generated")
+except Exception as err:
+    logger.exception(err)
+else:
+    logger.info("Manually register plone.dexterity.schema.generated utility")
